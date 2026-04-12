a979a1c607ccdea52a3850f1effa28a3a94460a9
panEuler <doerpan2002@gmail.com>
2026-04-12 15:34:03 +0800

    debug

--- src/biomol_surface_unsup/losses/__init__.py
+++ src/biomol_surface_unsup/losses/__init__.py
@@ -1,7 +1,7 @@
 """Loss functions."""
 
-from biomol_surface_unsup.losses.loss_builder import build_loss
+from biomol_surface_unsup.losses.loss_builder import build_loss_fn
 from biomol_surface_unsup.losses.vdw import lj_body_integral
 from biomol_surface_unsup.losses.pressure_volume import pressure_volume_loss
 
-__all__ = ["build_loss", "pressure_volume_loss", "lj_body_integral"]
+__all__ = ["build_loss_fn", "pressure_volume_loss", "lj_body_integral"]
